9216ee326c3813f869bf44282fd1c871e86b0831
Trần Hoàng Kim Anh <kimanhtranhoang@gmail.com>
2024-02-27 19:37:23 +0700

    Update StatisticsServlet.java

--- demo12/src/main/java/com/example/demo12/StatisticsServlet.java
+++ demo12/src/main/java/com/example/demo12/StatisticsServlet.java
@@ -1,12 +1,12 @@
 package com.example.demo12;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import jakarta.ejb.EJB;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,13 +17,24 @@ public class StatisticsServlet extends HttpServlet {
     @EJB
     private StatisticsBean statisticsBean;
 
+    @EJB
+    private UserSessionBean userSessionBean;
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        List<StatisticsBean.Input> inputs = statisticsBean.getInputs();
-        request.setAttribute("inputs", inputs);
+        HttpSession session = request.getSession();
+        UserSessionBean userSessionBean = (UserSessionBean) session.getAttribute("userSessionBean");
 
-        List<StatisticsBean.Output> outputs = statisticsBean.getOutputs();
-        request.setAttribute("outputs", outputs);
+        if (userSessionBean != null && userSessionBean.isLoggedIn()) {
+            List<StatisticsBean.Input> inputs = statisticsBean.getInputs();
+            request.setAttribute("inputs", inputs);
 
-        request.getRequestDispatcher("statistics.jsp").forward(request, response);
+            List<StatisticsBean.Output> outputs = statisticsBean.getOutputs();
+            request.setAttribute("outputs", outputs);
+
+            request.getRequestDispatcher("statistics.jsp").forward(request, response);
+        } else {
+            // Redirect to login page
+            response.sendRedirect("login.jsp");
+        }
     }
 }
